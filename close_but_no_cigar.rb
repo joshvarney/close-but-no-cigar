9962b96585849ea2c1096ba124333722fe3d327e
Josh Varney <joshcainvarney@gmail.com>
2018-06-25 11:18:59 -0400

    test that cigar is string

--- close_but_no_cigar.rb
+++ close_but_no_cigar.rb
@@ -1,4 +1,7 @@
-def cigar()
-	str1 = "1234"
-	str2 = "12345"
+str1 = "1234"
+str2 = "12345"
+
+def cigar(str1, str2)
+	str1
+	str2
 end
--- close_but_no_cigar_test.rb
+++ close_but_no_cigar_test.rb
@@ -2,10 +2,13 @@ require "minitest/autorun"
 require_relative "close_but_no_cigar.rb"
 class TestClose_but_no_cigar < Minitest::Test 
 
+	
 	def test_assert_that_1_equal_1
 		assert_equal(1, 1)
 	end
-	def test_assert_that_fuction_has_string
-		assert_equal(String,cigar().class)
+	def test_assert_that_str1_doesnt_equal_str2
+		str1 = "1234"
+		str2 = "12345"
+		assert_equal(String,cigar(str1,str2).class)
 	end
 end
