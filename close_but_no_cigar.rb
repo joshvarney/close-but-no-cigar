25c2f153d170bf226e23070fab749673b737656b
Josh Varney <joshcainvarney@gmail.com>
2018-06-27 13:26:38 -0400

    all tests pass everything refactored

--- close_but_no_cigar.rb
+++ close_but_no_cigar.rb
@@ -8,37 +8,39 @@ def cigar(myticket,winningtick)
 		unless ticket == winningtick[index]
 			counter += 1
 		end
-end
-	counter	    
+	end
+		counter	    
 end
 
 def cigar_array(mytickarray,winningtick)
-	return_array = []
+	return_array = Array.new
 	mytickarray.each do |ticket|
-		return_array << cigar(ticket, winningtick)
+	return_array << cigar(ticket, winningtick)
 	end
-  	return_array
+  		return_array
 end
 
 def winarr(mytickarray,winningtick)
-	warr = []
-	off_by = []
+	warr = Array.new
+	off_by = Array.new
 	winningtick.each do |ticket|
-		matches = cigar_array(mytickarray, ticket)
-		winners = 0
-		matches.each_with_index do |m|
-			if m == 0
-				winners += 1
-			elsif 
-				off_by << m
-			end 
-			  winners
-		end
-			warr << winners
+	matches = cigar_array(mytickarray, ticket)
+	winners = 0
+	matches.each_with_index do |m|
+		if m == 0
+			winners += 1
+		elsif 
+			off_by << m
+		end 
 	end
-	warr = warr.sort
-	warr << off_by.sort
-	warr
+		warr << winners
+	end
+		off_by = off_by.sort
+		warr = warr.sort
+		new_arr = Array.new
+		new_arr << warr
+		new_arr << off_by
+		p new_arr
 end
 
 
--- close_but_no_cigar_test.rb
+++ close_but_no_cigar_test.rb
@@ -21,12 +21,12 @@ class TestClose_but_no_cigar < Minitest::Test
 		assert_equal([0,2,2], cigar_array(["1234", "1222", "1245"], "1234"))
 	end
 	def test_assert_if_returns_equal_winning_ticket
-	 	assert_equal([1, 2, [2, 2, 2, 2, 2]], winarr(["1234", "1234", "1247", "1255"], ["1234", "1247"]))
+	 	assert_equal([[1, 2], [2, 2, 2, 2, 2]], winarr(["1234", "1234", "1247", "1255"], ["1234", "1247"]))
 	end
 	def test_assert_that_winning
-		assert_equal([1, [3, 4]], winarr(["1234", "4567", "4564"], ["1234"]))
+		assert_equal([[1], [3, 4]], winarr(["1234", "4567", "4564"], ["1234"]))
 	end
 	def test_assert_that_sorts_multiple_tickets
-		assert_equal([2, 4, [2, 2, 4, 4, 4, 4, 4, 4, 4, 4]], winarr(["1234", "1234", "4567", "1245", "1234", "4567", "1234", "4589"], ["1234", "4567"]))
+		assert_equal([[2, 4], [2, 2, 4, 4, 4, 4, 4, 4, 4, 4]], winarr(["1234", "1234", "4567", "1245", "1234", "4567", "1234", "4589"], ["1234", "4567"]))
 	end
   end
