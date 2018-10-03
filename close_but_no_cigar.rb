449a77e4f2b4e317188e53361583a1b2019d3c73
Josh Varney <joshcainvarney@gmail.com>
2018-10-03 16:14:10 -0400

    pushing with wv

--- close_but_no_cigar.rb
+++ close_but_no_cigar.rb
@@ -1,5 +1,3 @@
-
-
 def cigar(myticket,winningtick)
 	myticket = myticket.split("")
 	winningtick = winningtick.split("")
@@ -9,7 +7,7 @@ def cigar(myticket,winningtick)
 			counter += 1
 		end
 	end
-		counter	    
+	counter	    
 end
 
 def cigar_array(mytickarray,winningtick)
@@ -17,30 +15,30 @@ def cigar_array(mytickarray,winningtick)
 	mytickarray.each do |ticket|
 	return_array << cigar(ticket, winningtick)
 	end
-  		return_array
+  	return_array
 end
 
 def winarr(mytickarray,winningtick)
 	warr = Array.new
 	off_by = Array.new
 	winningtick.each do |ticket|
-	matches = cigar_array(mytickarray, ticket)
-	winners = 0
-	matches.each_with_index do |m|
-		if m == 0
-			winners += 1
-		elsif 
-			off_by << m
-		end 
-	end
+		matches = cigar_array(mytickarray, ticket)
+		winners = 0
+		matches.each_with_index do |m|
+			if m == 0
+				winners += 1
+			else 
+				off_by << m
+			end
+		end
 		warr << winners
 	end
-		off_by = off_by.sort
-		warr = warr.sort
-		new_arr = Array.new
-		new_arr << warr
-		new_arr << off_by
-		new_arr
+	off_by = off_by.sort
+	warr = warr.sort
+	new_arr = Array.new
+	new_arr << warr
+	new_arr << off_by
+	new_arr
 end
 
 
--- close_but_no_cigar_test.rb
+++ close_but_no_cigar_test.rb
@@ -2,7 +2,6 @@ require "minitest/autorun"
 require_relative "close_but_no_cigar.rb"
 class TestClose_but_no_cigar < Minitest::Test 
 
-	
 	def test_assert_that_1_equal_1
 		assert_equal(1, 1)
 	end
